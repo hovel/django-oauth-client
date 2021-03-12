9db051df636a54e0d9bdc355ac01f3d8c5fda10d
lampslave <lampslave@gmail.com>
2021-03-13 01:43:34 +0300

    add field is_installed to integration

--- oauth_client/admin.py
+++ oauth_client/admin.py
@@ -5,9 +5,9 @@ from oauth_client.models import ProviderPart, Integration
 
 @admin.register(ProviderPart)
 class ProviderPartAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['name', 'provider']
 
 
 @admin.register(Integration)
 class IntegrationAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['endpoint', 'is_installed', 'admin']
--- oauth_client/models.py
+++ oauth_client/models.py
@@ -18,12 +18,16 @@ class ProviderPart(models.Model):
     token_url = models.URLField(max_length=150, null=True, blank=True)
     provider = models.CharField(max_length=50, db_index=True)
 
+    def __str__(self):
+        return f'{self.name}'
+
 
 class Integration(models.Model):
     endpoint = models.CharField(max_length=255, unique=True)
     admin = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.SET_NULL,
         related_name='managed_integrations', blank=True, null=True)
+    is_installed = models.BooleanField(default=False)
 
     def __str__(self):
         return f'{self.endpoint}'
