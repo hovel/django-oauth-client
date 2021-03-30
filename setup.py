1da8cf5a0352b45da97480694910e75fcb44fa2f
lampslave <lampslave@gmail.com>
2021-03-30 21:05:44 +0300

    use setuptools

--- setup.py
+++ setup.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
-setup(
-    name='OAuth Client',
-    version='0.1',
-    description='',
-    author='',
-    author_email='',
-    url='https://github.com/hovel/django-oauth-client',
-    packages=['oauth_client'],
-)
+setup()
