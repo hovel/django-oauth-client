8ffec506c69d09442bfd2e3fb50c3293e3c9c267
aryadovoy <ryav.omsk@gmail.com>
2021-09-13 21:00:23 +0400

    Small fix

--- oauth_client/exceptions.py
+++ oauth_client/exceptions.py
@@ -1,7 +1,7 @@
-from rest_framework.exceptions import APIException
+from django.core.exceptions import Exception
 
 
-class LockIntegrationError(APIException):
+class LockIntegrationError(Exception):
     status_code = 500
     default_detail = 'Lock Integration is unsuccess.'
     default_code = "lock_integration_error"
